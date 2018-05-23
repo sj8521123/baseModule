79e542bdbf1975b37c06ff380145f86a2e4ee29d
sj <13658029734@163.com>
2018-05-23 18:07:06 +0800

    添加刷新

---
+++ app/src/main/java/com/sj/basemodule/DataAdapter.java
@@ -0,0 +1,24 @@
+package com.sj.basemodule;
+
+import android.support.annotation.LayoutRes;
+import android.support.annotation.Nullable;
+
+import com.chad.library.adapter.base.BaseQuickAdapter;
+import com.chad.library.adapter.base.BaseViewHolder;
+
+import java.util.List;
+
+/**
+ * Created by 13658 on 2018/5/23.
+ */
+
+public class DataAdapter extends BaseQuickAdapter<String,BaseViewHolder> {
+    public DataAdapter(@LayoutRes int layoutResId, @Nullable List<String> data) {
+        super(layoutResId, data);
+    }
+
+    @Override
+    protected void convert(BaseViewHolder helper, String item) {
+
+    }
+}
--- app/src/main/java/com/sj/basemodule/MainActivity.java
+++ app/src/main/java/com/sj/basemodule/MainActivity.java
@@ -1,8 +1,28 @@
 package com.sj.basemodule;
 
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.scwang.smartrefresh.layout.SmartRefreshLayout;
+import com.scwang.smartrefresh.layout.api.RefreshLayout;
+import com.scwang.smartrefresh.layout.listener.OnLoadMoreListener;
+import com.scwang.smartrefresh.layout.listener.OnRefreshListener;
 import com.sj.basemodule.base.BaseActivity;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+
 public class MainActivity extends BaseActivity {
+    @BindView(R.id.recyclerView)
+    RecyclerView recyclerView;
+    @BindView(R.id.refreshLayout)
+    SmartRefreshLayout refreshLayout;
+    DataAdapter mAdapter;
+    List<String> datas;
+
     @Override
     public int initLayout() {
         return R.layout.activity_main;
@@ -15,11 +35,35 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public void initLayoutView() {
+        datas = new ArrayList<>();
+        recyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this));
+        mAdapter = new DataAdapter(R.layout.activity_data_item, datas);
+        mAdapter.openLoadAnimation();
+        recyclerView.setAdapter(mAdapter);
 
+        refreshLayout.setOnRefreshListener(new OnRefreshListener() {
+            @Override
+            public void onRefresh(RefreshLayout refreshlayout) {
+                mAdapter.notifyDataSetChanged();
+                refreshlayout.finishRefresh(2000/*,false*/);//传入false表示刷新失败
+            }
+        });
+        refreshLayout.setOnLoadMoreListener(new OnLoadMoreListener() {
+            @Override
+            public void onLoadMore(RefreshLayout refreshlayout) {
+                mAdapter.notifyDataSetChanged();
+                refreshlayout.finishLoadMore(2000/*,false*/);//传入false表示加载失败
+            }
+        });
+        refreshLayout.autoRefresh();
     }
 
     @Override
     public void initLocalData() {
-
+        for (int i = 0; i < 100; i++) {
+            datas.add(i + "");
+        }
+        mAdapter.notifyDataSetChanged();
     }
+
 }
