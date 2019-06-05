89fbf84ad1cd402856fa2534fcf14ad8ac94c723
github_sj <13658029734@163.com>
2019-06-05 16:39:53 +0800

    hello

--- app/src/main/java/com/sj/basemodule/net/STGApi.java
+++ app/src/main/java/com/sj/basemodule/net/STGApi.java
@@ -5,6 +5,7 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.jakewharton.retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import com.sj.basemodule.base.MyApplication;
+import com.sj.basemodule.net.interceptor.HttpCacheInterceptor;
 import com.sj.basemodule.net.interceptor.HttpHeaderInterceptor;
 import com.sj.basemodule.net.interceptor.LoggerInterceptor;
 import com.sj.basemodule.util.CookieUtil;
@@ -73,77 +74,4 @@ public class STGApi {
         }
         return instance;
     }
-
-    //日志拦截器
-/*    private class LoggingInterceptor implements Interceptor {
-        @Override
-        public Response intercept(Chain chain) throws IOException {
-            //这个chain里面包含了request和response，所以你要什么都可以从这里拿
-            Request request = chain.request();
-            long t1 = System.nanoTime();//请求发起的时间
-            LogUtil.i("okhttp", String.format("发送请求 %s on %s%n%s",
-                    request.url(), chain.connection(), request.headers()));
-            Response response = chain.proceed(request);
-            long t2 = System.nanoTime();//收到响应的时间
-            ResponseBody responseBody = response.peekBody(1024 * 1024);
-            LogUtil.i("okhttp", String.format(Locale.ENGLISH, "接收响应: [%s] %n返回json:【%s】 %.1fms%n%s",
-                    response.request().url(),
-                    responseBody.string(),
-                    (t2 - t1) / 1e6d,
-                    response.headers()));
-
-            return response;
-        }
-    }*/
-
-    //  添加请求头的拦截器
-   /* private class HttpHeaderInterceptor implements Interceptor {
-        @Override
-        public Response intercept(Chain chain) throws IOException {
-            //  也可以统一配置用户名
-            Request.Builder builder = chain.request().newBuilder()
-                    .header("Content-Type", "application/x-www-form-urlencoded")
-                    .header("Content-Type-Charset", "utf-8")
-                    .header("Accept", "application/json");
-            if (isRefreshToken) {
-                isRefreshToken = false;
-                *//*String token = SPUtils.getInstance(MyApplication.currentUserPrefsName).getString(KeyAndValueUserPrefs.Key.ACCESS_TOKEN);
-                builder.addHeader("token", token);*//*
-            } else {
-                builder.removeHeader("token");
-            }
-            return chain.proceed(builder.build());
-        }
-    }*/
-
-    //  配置缓存的拦截器
-    private class HttpCacheInterceptor implements Interceptor {
-
-        @Override
-        public Response intercept(Chain chain) throws IOException {
-            Request request = chain.request();
-            if (!NetWorkUtil.isConnected()) {  //没网强制从缓存读取
-                request = request.newBuilder()
-                        .cacheControl(CacheControl.FORCE_CACHE)
-                        .build();
-                LogUtil.d("Okhttp", "no network");
-            }
-            Response originalResponse = chain.proceed(request);
-            if (NetWorkUtil.isConnected()) {
-                // TODO: 2017/12/12  有网的时候读接口上的@Headers里的配置，你可以在这里进行统一的设置
-                //写入到配置中去
-                String cacheControl = request.cacheControl().toString();
-                return originalResponse.newBuilder()
-                        .header("Cache-Control", cacheControl)
-                        .removeHeader("Pragma")
-                        .build();
-            } else {
-                return originalResponse.newBuilder()
-                        .header("Cache-Control", "public, only-if-cached, max-stale=2419200")
-                        .removeHeader("Pragma")
-                        .build();
-            }
-        }
-    }
-
 }
