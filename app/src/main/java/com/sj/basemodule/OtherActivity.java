101fcbfa6c10d4e1fde14b041f711b99d78883c9
github_sj <13658029734@163.com>
2019-09-02 17:54:30 +0800

    1、添加recycleView 的itemDecoraTion

--- app/src/main/java/com/sj/basemodule/OtherActivity.java
+++ app/src/main/java/com/sj/basemodule/OtherActivity.java
@@ -5,25 +5,20 @@ import android.os.Bundle;
 import android.view.View;
 
 import com.chad.library.adapter.base.BaseQuickAdapter;
-import com.chad.library.adapter.base.listener.OnItemChildClickListener;
 import com.chad.library.adapter.base.listener.OnItemClickListener;
 import com.sj.basemodule.adapter.TestAdapter;
 
 import java.util.ArrayList;
-import java.util.Observable;
-import java.util.Random;
-import java.util.concurrent.TimeUnit;
 
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.LinearSmoothScroller;
 import androidx.recyclerview.widget.RecyclerView;
 import basemodule.sj.com.basic.base.BaseActivity;
 import basemodule.sj.com.basic.util.ToastUtil;
+import basemodule.sj.com.basic.weight.decoration.GridItemDecoration;
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.functions.Consumer;
-import io.reactivex.schedulers.Schedulers;
 
 public class OtherActivity extends BaseActivity {
     private static final String TAG = "OtherActivity";
@@ -55,14 +50,26 @@ public class OtherActivity extends BaseActivity {
     @Override
     public void initLayoutView() {
         mDatas = new ArrayList<>();
-        LinearLayoutManager manager = new LinearLayoutManager(this);
-        mRecycleView.setLayoutManager(manager);
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(this,3);
+        // mRecycleView.addItemDecoration(new GridSectionAverageGapItemDecoration(5,5,5,5));
+        /* LinearLayoutManager manager = new LinearLayoutManager(this);*/
+        mRecycleView.setLayoutManager(gridLayoutManager);
+        //mRecycleView.addItemDecoration(new GridSectionAverageGapItemDecoration(5,5,5,5));
+        mRecycleView.addItemDecoration(new GridItemDecoration(this, R.drawable.list_divider_vertical, LinearLayoutManager.VERTICAL));
+        mRecycleView.addItemDecoration(new GridItemDecoration(this, R.drawable.list_divider_horizontal, LinearLayoutManager.HORIZONTAL));
         mAdapter = new TestAdapter(R.layout.test_item, mDatas);
         mRecycleView.setAdapter(mAdapter);
-        LinearSmoothScroller mScroller = new TopSmoothScroller(this);
 
+
+        LinearSmoothScroller mScroller = new TopSmoothScroller(this);
+        for (int i = 0; i < 50; i++) {
+            mAdapter.getData().add(new String("helloWorld" + i));
+        }
+        mAdapter.notifyDataSetChanged();
+        mScroller.setTargetPosition(10);
+        mRecycleView.getLayoutManager().startSmoothScroll(mScroller);
         /*mAdapter.onAttachedToRecyclerView(mRecycleView);*/
-        io.reactivex.Observable.timer(5, TimeUnit.SECONDS)
+       /* io.reactivex.Observable.timer(5, TimeUnit.SECONDS)
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(new Consumer<Long>() {
@@ -75,10 +82,10 @@ public class OtherActivity extends BaseActivity {
                         // manager.scrollToPositionWithOffset(10, 0);
                         mScroller.setTargetPosition(10);
                         mRecycleView.getLayoutManager().startSmoothScroll(mScroller);
-                        /*mRecycleView.smoothScrollToPosition(10);*/
+                        *//*mRecycleView.smoothScrollToPosition(10);*//*
 
                     }
-                });
+                });*/
         mRecycleView.addOnItemTouchListener(new OnItemClickListener() {
             @Override
             public void onSimpleItemClick(BaseQuickAdapter adapter, View view, int position) {
