c9690a001af876a32131802fdc6ad5da4ab943ed
github_sj <13658029734@163.com>
2019-12-27 14:55:20 +0800

    1、xxxxxxxx

--- app/src/main/java/com/sj/basemodule/MainActivity.java
+++ app/src/main/java/com/sj/basemodule/MainActivity.java
@@ -2,14 +2,14 @@ package com.sj.basemodule;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.ArrayMap;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
-import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.Switch;
+import android.widget.Toast;
 
-import com.google.gson.Gson;
 import com.hjq.toast.ToastUtils;
 import com.sj.basemodule.mine.A_MineBaseInfoFragment;
 import com.sj.basemodule.mine.B_MineBaseInfoFragment;
@@ -22,8 +22,6 @@ import net.lucode.hackware.magicindicator.buildins.circlenavigator.CircleNavigat
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import androidx.viewpager.widget.ViewPager;
 import basemodule.sj.com.basic.adapter.HomePagerAdapter;
@@ -43,20 +41,12 @@ public class MainActivity extends BaseActivity {
     private static final String TAG = "MainActivity";
     private static final String[] TITLES = new String[]{"日常", "专项", "工作台", "主体"};
     @BindView(R.id.switch2)
-    Switch switch2;
+    Switch switch3;
     private List<String> mTitleDataList = Arrays.asList(TITLES);
     private HomePagerAdapter mPagerAdapter;
-    Student student = new Student();
-    StringBuilder x = new StringBuilder();
 
     @Override
     protected void reConnect() {
-        new Button(this).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
         ToastUtils.show("reConnect");
     }
 
@@ -75,12 +65,6 @@ public class MainActivity extends BaseActivity {
      */
     @Override
     public void initFromData() {
-        x.append(10);
-        System.out.println("修改值之前:" + x.toString());
-        System.out.println(student.test(x));
-        System.out.println("修改值之后:" + x.toString());
-        Log.i(TAG, "initFromData: " + "aa.javacc".matches(".*\\.java$"));
-        System.out.println(0.0 / 0.0 == 0.0 / 0.0);
     }
 
     @Override
@@ -91,40 +75,6 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public void initLocalData() {
-        Pattern p = Pattern.compile("[a-z]{3,10}[0-9]");
-        String s = "1234aaaaa678911a";//10个字符
-        Matcher m = p.matcher(s);
-
-        if (m.find()) {
-            System.out.println(m.start() + " - " + m.end());
-        } else {
-            System.out.println("not match!");
-        }
-        String str = "[家庭联系人信息不符合]";
-        List<String> str2 = new Gson().fromJson(str, List.class);
-       /* Pattern p = Pattern.compile("\\.*");//正则表达式为3~5个数字跟上两个字母
-        String s = "123aaa-5423zxx-642oii-00";
-        String s2 = "....";
-        Matcher m = p.matcher(s);*/
-       /* while(m.find()){
-            p(m.group(2));
-        }*/
-        //CASE_INSENSITIVE忽略大小写
-       /* Pattern p = Pattern.compile("java", Pattern.CASE_INSENSITIVE);
-        Matcher m = p.matcher("java Java JAVA JAva I love Java and you ?");
-        StringBuffer sb = new StringBuffer();
-        int index = 1;
-        while (m.find()) {
-            //m.appendReplacement(sb, (index++ & 1) == 0 ? "java" : "JAVA"); 较为简洁的写法
-            if ((index & 1) == 0) {//偶数
-                m.appendReplacement(sb, "java");
-            } else {
-                m.appendReplacement(sb, "JAVA");
-            }
-            index++;
-        }
-        m.appendTail(sb);//把剩余的字符串加入
-        p(sb);*/
     }
 
     private Void test() {
@@ -132,8 +82,31 @@ public class MainActivity extends BaseActivity {
     }
 
     private void initViewPage() {
+        ArrayMap<String, String> arrayMap = new ArrayMap<>();
+        Toast.makeText(this,"he",Toast.LENGTH_LONG).show();
+        initPager();
+        switch3.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    ToastUtils.show("打开");
+                } else {
+                    ToastUtils.show("关闭");
+                }
+            }
+        });
+        switch3.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_UP) {
+                    switch3.setChecked(switch3.isChecked());
+                }
+                return false;
+            }
+        });
+    }
 
-
+    private void initPager() {
         mPagerAdapter = new HomePagerAdapter(getSupportFragmentManager());
 
         //基础信息
@@ -149,25 +122,6 @@ public class MainActivity extends BaseActivity {
         mViewPager.setPageTransformer(true, new AlphaAndScalePageTransformer());
 
         mViewPager.setAdapter(mPagerAdapter);
-        switch2.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    ToastUtils.show("打开");
-                } else {
-                    ToastUtils.show("关闭");
-                }
-            }
-        });
-        switch2.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                if (event.getAction() == MotionEvent.ACTION_UP) {
-                    switch2.setChecked(switch2.isChecked());
-                }
-                return false;
-            }
-        });
     }
 
     private void initMagicIndicator() {
--- app/src/main/java/com/sj/basemodule/Student.java
+++ app/src/main/java/com/sj/basemodule/Student.java
@@ -4,14 +4,7 @@ public class Student {
     public StringBuilder test(StringBuilder x) {
         Integer i = Integer.valueOf(x.toString());
         x.setLength(0);
-        x.append(i+2);
+        x.append(i + 2);
         return x;
-
     }
-    /*private static class singleHander{
-        private static Student student = new Student();
-    }
-    public static Student getInstance(){
-        return  singleHander.student;
-    }*/
 }
