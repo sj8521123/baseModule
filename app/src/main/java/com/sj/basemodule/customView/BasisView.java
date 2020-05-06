edaa4ae2a8d2b586401aac8fac33cad733a2cb0a
shijun <shijun@Tinman.cn>
2020-05-06 16:26:43 +0800

    xxxx

---
+++ app/src/main/java/com/sj/basemodule/customView/BasisView.java
@@ -0,0 +1,50 @@
+package com.sj.basemodule.customView;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+/**
+ * @author shijun
+ * <p>
+ * Created on 2020/4/27.
+ */
+public class BasisView extends View {
+    public BasisView(Context context) {
+        super(context);
+    }
+
+    public BasisView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public BasisView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        super.onLayout(changed, left, top, right, bottom);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        Paint paint = new Paint();
+        paint.setColor(Color.RED);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeWidth(50);
+
+        canvas.drawCircle(190,200,150,paint);
+    }
+}
